113b45b0ea8d1ea9a5113c2bee4ef434af69c17d
saasbook <saasbook@saasbook.(none)>
2014-04-08 15:20:49 -0300

    added rails files

---
+++ buffbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Buffbook::Application.config.secret_key_base = 'f623415dc2fd00f2564d89ec4e5398b78aaa7c639e1ec8f1271d48e6e5a2ba900d1ac2f41bd8418035c91c2d61c133204eb06fc6f5a6fcbb5d3743b2c35bc45a'
---
+++ buffbook/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Buffbook::Application.config.session_store :cookie_store, key: '_buffbook_session'
